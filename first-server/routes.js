3f5e42e91fc9f24091218fb32bf0f2a185a4a68b
Glaydston Veloso <glaydston.oliveira@gmail.com>
2014-03-16 11:11:44 -0300

    Working with 'fs'

--- first-server/routes.js
+++ first-server/routes.js
@@ -1,5 +1,4 @@
 var http = require('http');
-
 var server = http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text/html'});
     if(request.url == '/'){
@@ -12,6 +11,23 @@ var server = http.createServer(function(request, response){
     response.end();
 });
 
+
+ var http = require('http');
+ var server = http.createServer(function(request, response){
+     response.writeHead(200, {'Content-Type':'text/html'});
+     if(request.url == '/'){
+	 response.write('<h1>Main Page</h1>');
+     } else if(request.url == '/welcome') {
+	 response.write('<h1>Welcome :)</h1>');
+     } else {
+	 response.write('Page not found!!');
+     }
+     response.end();
+ });
+
+ server.listen(3000, function(){
+     console.log("It's working");
+ });
 server.listen(3000, function(){
     console.log("It's working");
 });
