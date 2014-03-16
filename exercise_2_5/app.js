aaf978352ada8ad8fe0ac442b01534db295be33b
Glaydston Veloso <glaydston.oliveira@gmail.com>
2014-03-16 12:17:57 -0300

    optimization of functions

--- exercise_2_5/app.js
+++ exercise_2_5/app.js
@@ -2,27 +2,23 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+function readFile(name, response){
+    fs.readFile(__dirname + "/" + name + ".html", function(err, html){
+	response.writeHead(200, {'Content-Type':'text/html'});
+	response.write(html);
+	response.end();
+    });
+}
+
 var server = http.createServer(function(request, response){
     var result = url.parse(request.url, true);
     
     if(result.pathname ==  '/contato'){
-	fs.readFile(__dirname + "/contato.html", function(err, html){
-	    response.writeHead(200, {'Content-Type':'text/html'});
-	    response.write(html);
-	    response.end();
-	});
-    } else if(result.pathname == '/artigos'){
-	fs.readFile(__dirname + "/artigos.html", function(err, html){
-	    response.writeHead(200, {'Content-Type': 'text/html'});
-	    response.write(html);
-	    response.end();
-	});
+	readFile('contato', response);
+    } else if(result.pathname == '/artigos' || result.pathname == '/'){
+	readFile('artigos', response);
     } else {
-	fs.readFile(__dirname + "/erro.html", function(err, html){
-	    response.writeHead(200, {'Content-Type':'text/html'});
-	    response.write(html);
-	    response.end();
-	});
+	readFile('erro', response);
     }
 
 });
